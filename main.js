ca07b10dc459bf8d9ddbce39e62e1ebfcf452a17
unknown <kvamc1101@gmail.com>
2023-08-25 19:12:44 +0530

    added delete functionality

--- main.js
+++ main.js
@@ -23,15 +23,20 @@ function onSubmit(e) {
     // Create new list item with user
     const li = document.createElement('li');
 
+    // creating deleteBtn
+    var newBtn = document.createElement('button');
+    newBtn.className = 'delete btnStyle';
+    newBtn.appendChild(document.createTextNode('Delete'));
+
     // Add text node with input values
     li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
-
+    li.appendChild(newBtn);
 
 
     // Append to ul
     userList.appendChild(li);
 
-    // Stroing into local Storage
+    // Storing into local Storage
     // localStorage.setItem(nameInput.value,emailInput.value);
     // console.log(localStorage.getItem('vamshi'));
 
@@ -40,7 +45,6 @@ function onSubmit(e) {
         name : nameInput.value,
         email : emailInput.value
     };
-    console.log(myobj);
     var myobj_serialized = JSON.stringify(myobj);
     localStorage.setItem(myobj.email,myobj_serialized);
 
@@ -49,3 +53,24 @@ function onSubmit(e) {
     emailInput.value = '';
   }
 }
+
+// adding delete event
+var listItems = document.getElementById('users')
+listItems.addEventListener('click',removeItem);
+
+const keys = Object.keys(localStorage);
+function removeItem(e){
+    e.preventDefault();
+    if(e.target.classList.contains('delete')){
+            var li = e.target.parentElement;
+            var a = li.firstChild.textContent
+            a = a.split(":")
+            check = a[1].trim()
+            localStorage.removeItem(check);
+            listItems.removeChild(li);
+            
+    }
+}
+
+
+
